bd42d7e85bc7c3b5cc69f055c46835fd24b05495
Sebastian Termen <sebastian.termen@gmail.com>
2023-11-16 00:03:02 +0200

    Cleaning up source code for simpler implementation.

--- Source/Sandbox/main.cpp
+++ Source/Sandbox/main.cpp
@@ -1,6 +1,6 @@
 #include <durlib.h>
 
-#include "Core/GameInitializer.h"
+// #include "Core/GameInitializer.h"
 
 int main()
 {
@@ -9,7 +9,7 @@ int main()
     CLI_INFO("Welcome to Omni Grid Sandbox!");
 
     // All the games are initialized in this class
-    Sandbox::GameInitializer::Start();
+    // Sandbox::GameInitializer::Start();
 
     return 0;
 }
