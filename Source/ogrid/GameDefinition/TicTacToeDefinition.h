65330dc02f91838c065429a6b4420010143a2341
Sebastian <Sebastian.Termen@gmail.com>
2023-11-19 15:47:38 +0200

    This will most likely be removed as this has already been abstracted in some way.

--- Source/ogrid/GameDefinition/TicTacToeDefinition.h
+++ Source/ogrid/GameDefinition/TicTacToeDefinition.h
@@ -2,34 +2,34 @@
 
 #include <memory>
 
-#include "Grid/Grid.h"
-#include "MoveType.h"
-#include "MoveRules.h"
-#include "Player.h"
-#include "AbstractGameLogic.h"
+// #include "Grid/Grid.h"
+// #include "MoveType.h"
+// #include "MoveRules.h"
+// #include "Player.h"
+// #include "AbstractGameLogic.h"
 
 namespace OGRID
 {
-    class TicTacToeDefinition : public AbstractGameLogic
-    {
-    private:
-        // Constructor and Destructor
-    public:
-        TicTacToeDefinition();
-        ~TicTacToeDefinition();
-        // Private methods
-    private:
-        void InitializeGame() override;
-        void DeinitializeGame() override;
-        // Public methods
-    public:
-        void MakeMove(Player &player) override;
+    // class TicTacToeDefinition : public AbstractGameLogic
+    // {
+    // private:
+    //     // Constructor and Destructor
+    // public:
+    //     TicTacToeDefinition();
+    //     ~TicTacToeDefinition();
+    //     // Private methods
+    // private:
+    //     void InitializeGame() override;
+    //     void DeinitializeGame() override;
+    //     // Public methods
+    // public:
+    //     void MakeMove(Player &player) override;
 
-        void CheckConditions() override;
+    //     void CheckConditions() override;
 
-        void ResetGame() override;
+    //     void ResetGame() override;
 
-        void EndGame() override;
-    };
+    //     void EndGame() override;
+    // };
 
 }
