24fd64eca336e24e21a9f4e75bc6217bf0786c1c
Sebastian <Sebastian.Termen@gmail.com>
2023-11-19 23:21:22 +0200

    Oops.

---
+++ Source/Sandbox/Core/GameInitializer.cpp
@@ -0,0 +1,31 @@
+#include "GameInitializer.h"
+
+#include <durlib.h>
+#include <ogrid.h>
+
+#include "GUI/GameWindow.h"
+
+namespace Sandbox
+{
+    void GameInitializer::Start()
+    {
+        int choice = 0;
+        while (choice == 0)
+        {
+            CLI_TRACE("\n1. TicTacToe\n2. Connect Four");
+            choice = DURLIB::GIBI(1, 2);
+        }
+        switch (choice)
+        {
+        case 1:
+            GameWindow<OGRID::TicTacToe>().Start();
+            break;
+        case 2:
+            // GameWindow<OGRID::ConnectFour>().Start();
+            break;
+        default:
+            CLI_TRACE("Invalid choice");
+            break;
+        }
+    }
+}
---
+++ Source/Sandbox/Core/GameInitializer.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace Sandbox
+{
+    class GameInitializer
+    {
+    public:
+        // Starts the app and gives choice of games
+        static void Start();
+    };
+}
