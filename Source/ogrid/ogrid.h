eaad6831cc36d668dedf72a1920e09b03994bd7e
Sebastian Termen <sebastian.termen@gmail.com>
2023-11-19 13:29:28 +0200

    Using relative paths to segregate projects (library/executable).

--- Source/ogrid/ogrid.h
+++ Source/ogrid/ogrid.h
@@ -5,5 +5,5 @@
 #include "GameLogic/IGame.h"
 #include "Player/Player.h"
 #include "Player/Moves.h"
-#include <Games/TicTacToe.h>
-#include <Games/ConnectFour.h>
+#include "Games/TicTacToe.h"
+#include "Games/ConnectFour.h"
