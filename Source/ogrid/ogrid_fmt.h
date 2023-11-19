cea8986cd35b7723f418e9cdcb457961129c888b
Sebastian <Sebastian.Termen@gmail.com>
2023-11-19 17:12:35 +0200

    Added a fmt specific header, as it was impossible to keep the code within the needed header/source/class location due to circular dependencies.

---
+++ Source/ogrid/ogrid_fmt.h
@@ -0,0 +1,85 @@
+#pragma once
+
+#include <string>
+
+#include "fmt/format.h"
+
+#include "Grid/Grid.h"
+#include "Player/Player.h"
+#include "GameDefinition/Piece.h"
+
+// Formatting for fmt library.
+
+// Grid formatting
+template <>
+struct fmt::formatter<OGRID::Grid> : fmt::formatter<std::string>
+{
+    // Parses format specifications of the form '[:...]' which you can ignore.
+    constexpr auto parse(format_parse_context &ctx) { return ctx.begin(); }
+
+    // Formats the Grid using provided format specifiers.
+    template <typename FormatContext>
+    auto format(const OGRID::Grid &grid, FormatContext &ctx)
+    {
+        // Use a memory buffer to store the temporary output.
+        fmt::memory_buffer buf;
+
+        fmt::format_to(std::back_inserter(buf), "\n");
+
+        for (int i = 0; i < grid.GetRows(); i++)
+        {
+            for (int j = 0; j < grid.GetCols(); j++)
+            {
+                OGRID::Cell *cell = grid.GetGrid()[i][j];
+
+                // Assuming you want a space between characters in a row.
+                if (j > 0)
+                    fmt::format_to(std::back_inserter(buf), " ");
+                // Check if the cell pointer is not null and then access m_Piece
+                if (cell != nullptr && cell->m_Piece != nullptr)
+                    fmt::format_to(std::back_inserter(buf), "{}", cell->m_Piece->GetRepresentation());
+                else
+                    fmt::format_to(std::back_inserter(buf), " ");
+                // fmt::format_to(std::back_inserter(buf), "{}", grid.GetGrid()[i][j]);
+            }
+            // Add a newline after each row, except the last one.
+            if (i < grid.GetRows() - 1)
+                fmt::format_to(std::back_inserter(buf), "\n");
+        }
+
+        // Output the buffer to the formatting context and return the iterator.
+        return fmt::format_to(ctx.out(), "{}", to_string(buf));
+    }
+};
+
+// Player formatting
+template <>
+struct fmt::formatter<OGRID::PlayerType> : formatter<std::string>
+{
+    template <typename FormatContext>
+    auto format(OGRID::PlayerType p, FormatContext &ctx)
+    {
+        std::string name = p == OGRID::PlayerType::Human ? "Human" : "AI";
+        return formatter<std::string>::format(name, ctx);
+    }
+};
+
+template <>
+struct fmt::formatter<OGRID::Player> : fmt::formatter<std::string>
+{
+    // Parses format specifications of the form '[:...]' which you can ignore.
+    constexpr auto parse(format_parse_context &ctx) { return ctx.begin(); }
+
+    // Formats the Player using provided format specifiers.
+    template <typename FormatContext>
+    auto format(const OGRID::Player &player, FormatContext &ctx)
+    {
+        // Use a memory buffer to store the temporary output.
+        fmt::memory_buffer buf;
+
+        fmt::format_to(std::back_inserter(buf), "{} | {}", player.GetPlayerName(), player.GetPlayerType());
+
+        // Output the buffer to the formatting context and return the iterator.
+        return fmt::format_to(ctx.out(), "{}", to_string(buf));
+    }
+};
