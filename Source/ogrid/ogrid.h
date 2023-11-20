989a9163328780be58e1158dbdd1087ea59be917
LastWeekNextDay <doesn@mail.ru>
2023-11-20 19:43:23 +0200

    Added needed files to ogrid.h

--- Source/ogrid/ogrid.h
+++ Source/ogrid/ogrid.h
@@ -14,9 +14,13 @@
 #include "GameLogicImplementation/GameStateExtensions.h"
 #include "GameLogicImplementation/GameStateChecker.h"
 #include "GameLogicImplementation/GameConfiguration.h"
+#include "GameLogicImplementation/PieceMoveRules.h"
 
 // Games
 // #include "Games/ogrid_games.h"
 #include "Games/TicTacToe/TicTacToeStateCheck.h"
 #include "Games/TicTacToe/TicTacToePieces.h"
 #include "Games/TicTacToe/TicTacToe.h"
+#include "Games/ConnectFour/ConnectFourStateCheck.h"
+#include "Games/ConnectFour/ConnectFourPieces.h"
+#include "Games/ConnectFour/ConnectFour.h"
