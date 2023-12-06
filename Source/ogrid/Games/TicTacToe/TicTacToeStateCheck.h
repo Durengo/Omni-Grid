868ec27ea3535ff0d6aacd8435922b1eedf434ae
Sebastian <Sebastian.Termen@gmail.com>
2023-12-06 22:14:28 +0200

    Documentation for Tic Tac Toe.

--- Source/ogrid/Games/TicTacToe/TicTacToeStateCheck.h
+++ Source/ogrid/Games/TicTacToe/TicTacToeStateCheck.h
@@ -3,15 +3,39 @@
 #include "GameLogicInterface/IGameState.h"
 #include "GameLogicImplementation/GameStateExtensions.h"
 
+/**
+ * @file TicTacToeStateCheck.h
+ * @brief TicTacToe state check
+ * @date 2023-12-06
+ */
+
 namespace OGRID
 {
     class TicTacToeStateCheck : public IGameState
     {
     private:
+        /**
+         * @brief TicTacToe state check
+         * @date 2023-12-06
+         */
         GameStateExtensions m_GameStateExtensions = GameStateExtensions();
 
     public:
+        /**
+         * @brief Check if the game has a winning condition
+         * @date 2023-12-06
+         * @param grid Grid to check
+         * @return the side that won
+         * @see Player
+         */
         int CheckWin(Grid *grid) const override;
+
+        /**
+         * @brief Check if the game has a draw condition
+         * @date 2023-12-06
+         * @param grid Grid to check
+         * @return true If the game has a draw condition, false otherwise
+         */
 
         bool IsDraw(Grid *grid) const override;
     };
