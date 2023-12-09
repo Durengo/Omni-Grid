ce004df0fbadada883a7c24105e0c7e94782eca3
Sebastian <Sebastian.Termen@gmail.com>
2023-12-09 11:34:01 +0200

    UserMenu class startup.

---
+++ Source/Sandbox/UserMenu/UserMenu.cpp
@@ -0,0 +1,20 @@
+#include "UserMenu.h"
+
+/**
+ * @file UserMenu.cpp
+ * @brief Contains the UserMenu class
+ * @date 2023-12-06
+ */
+
+namespace Sandbox
+{
+
+    UserMenu::UserMenu()
+    {
+    }
+
+    UserMenu::~UserMenu()
+    {
+    }
+
+}
---
+++ Source/Sandbox/UserMenu/UserMenu.h
@@ -0,0 +1,20 @@
+#pragma once
+
+/**
+ * @file UserMenu.h
+ * @brief Contains the UserMenu class
+ * @date 2023-12-06
+ */
+
+namespace Sandbox
+{
+
+    class UserMenu
+    {
+    private:
+    public:
+        UserMenu();
+        ~UserMenu();
+    };
+
+}
