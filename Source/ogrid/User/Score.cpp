0ccd84a8a25c1d97e22349625022266146099532
Sebastian <Sebastian.Termen@gmail.com>
2023-12-09 11:12:41 +0200

    Score class definitions and implementation.

--- Source/ogrid/User/Score.cpp
+++ Source/ogrid/User/Score.cpp
@@ -4,5 +4,44 @@ namespace OGRID
 {
     // Constructors & Destructors
 
+    Score::Score()
+        : m_Wins(0), m_Losses(0), m_WinRate(0.0f)
+    {
+    }
+
+    Score::~Score()
+    {
+    }
+
     // Getters & Setters
+
+    unsigned int Score::GetWins() const
+    {
+        return m_Wins;
+    }
+
+    void Score::SetWins(unsigned int wins)
+    {
+        m_Wins = wins;
+    }
+
+    unsigned int Score::GetLosses() const
+    {
+        return m_Losses;
+    }
+
+    void Score::SetLosses(unsigned int losses)
+    {
+        m_Losses = losses;
+    }
+
+    float Score::GetWinRate() const
+    {
+        return m_WinRate;
+    }
+
+    void Score::SetWinRate(float winRate)
+    {
+        m_WinRate = winRate;
+    }
 }
--- Source/ogrid/User/Score.h
+++ Source/ogrid/User/Score.h
@@ -12,9 +12,24 @@ namespace OGRID
     class Score
     {
     private:
+        unsigned int m_Wins;
+        unsigned int m_Losses;
+        float m_WinRate;
+
         // Constructors & Destructors
     public:
+        Score();
+        ~Score();
+
         // Getters & Setters
     public:
+        unsigned int GetWins() const;
+        void SetWins(unsigned int wins);
+
+        unsigned int GetLosses() const;
+        void SetLosses(unsigned int losses);
+
+        float GetWinRate() const;
+        void SetWinRate(float winRate);
     };
 }
