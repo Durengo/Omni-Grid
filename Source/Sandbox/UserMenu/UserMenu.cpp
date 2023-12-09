ce336c1db3c5ae2ac33b41309109d6a5032fbf60
Sebastian <Sebastian.Termen@gmail.com>
2023-12-09 12:19:04 +0200

    A class which will serve as an abstraction to interact with the database and provide login/register functionality as well as view the user's score.

--- Source/Sandbox/UserMenu/UserMenu.cpp
+++ Source/Sandbox/UserMenu/UserMenu.cpp
@@ -1,20 +1,45 @@
 #include "UserMenu.h"
 
-/**
- * @file UserMenu.cpp
- * @brief Contains the UserMenu class
- * @date 2023-12-06
- */
-
 namespace Sandbox
 {
 
     UserMenu::UserMenu()
+        : m_Database(nullptr)
     {
     }
 
     UserMenu::~UserMenu()
     {
+    }
+
+    void UserMenu::Start()
+    {
+        m_Database = new SQLWRAP::database("omni-grid.db");
+
+        FirstRun();
+    }
+
+    void UserMenu::FirstRun()
+    {
+        std::string createUserTable = "CREATE TABLE IF NOT EXISTS User("
+                                      "UserId INTEGER PRIMARY KEY,"
+                                      "UserName TEXT NOT NULL,"
+                                      "UserPassword TEXT NOT NULL,"
+                                      "UserFirstName TEXT,"
+                                      "UserLastName TEXT);";
+
+        std::string createScoreTable = "CREATE TABLE IF NOT EXISTS Score("
+                                       "ScoreId INTEGER PRIMARY KEY,"
+                                       "UserId INTEGER,"
+                                       "Wins INTEGER NOT NULL,"
+                                       "Losses INTEGER NOT NULL,"
+                                       "WinRate REAL NOT NULL,"
+                                       "FOREIGN KEY(UserId) REFERENCES User(UserId));";
+
+        m_Database->createTableFromString(createUserTable);
+        m_Database->createTableFromString(createScoreTable);
+
+        m_Database->displayAllTables();
     }
 
 }
--- Source/Sandbox/UserMenu/UserMenu.h
+++ Source/Sandbox/UserMenu/UserMenu.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <Tools/Database/database.h>
+
 /**
  * @file UserMenu.h
  * @brief Contains the UserMenu class
@@ -12,9 +14,18 @@ namespace Sandbox
     class UserMenu
     {
     private:
+        SQLWRAP::database *m_Database;
+
     public:
         UserMenu();
         ~UserMenu();
+
+        void Start();
+        void FirstRun();
+        // void Login();
+        // void Register();
+        // void MainMenu();
+        // void Play();
     };
 
 }
